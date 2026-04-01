790e0cdcb45a9979bcb621c0fd1026946a0086fe
Vinicius Lazaroti <viniciuslazarotti@gmail.com>
2026-04-01 15:56:09 -0300

    Alg8.java

--- algoritmos/Alg8.java
+++ algoritmos/Alg8.java
@@ -1,1 +1,28 @@
+import java.util.Scanner;
 
+public class Alg3 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Este programa aceita apenas números inteiros");
+
+        System.out.println("Insira o primeiro número: ");
+        int primeiroNumero = teclado.nextInt();
+        System.out.println("Insira o segundo número: ");
+        int segundoNumero = teclado.nextInt();
+        System.out.println("Insira o terceiro número: ");
+        int terceiroNumero = teclado.nextInt();
+        System.out.println("Insira o quarto número: ");
+        int quartoNumero = teclado.nextInt();
+        System.out.println("Insira o quinto número: ");
+        int quintoNumero = teclado.nextInt();
+
+        int soma = primeiroNumero + segundoNumero  + terceiroNumero + quartoNumero + quintoNumero;
+        int media = (primeiroNumero + segundoNumero  + terceiroNumero + quartoNumero + quintoNumero) / 5;
+
+        System.out.println("Soma: " + soma);
+        System.out.println("Média: " + media);
+      
+        teclado.close();
+    }
+}
