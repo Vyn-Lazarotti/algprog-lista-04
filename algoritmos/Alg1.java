3644e17dbe8e5cf2b7704ce694307838db3671e1
Vinicius Lazaroti <viniciuslazarotti@gmail.com>
2026-04-01 14:21:37 -0300

    Alg1.java

--- algoritmos/Alg1.java
+++ algoritmos/Alg1.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
 
+public class Alg1 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int nota;
+        do {
+            System.out.println("Digite um número inteiro entre 0 e 10: ");
+            nota = teclado.nextInt();
+            if (nota < 0 || nota > 10){
+                System.out.println("Informe um valor válido");
+            }
+        } while (nota < 0 || nota > 10);
+        
+
+        teclado.close();
+    }
+}
