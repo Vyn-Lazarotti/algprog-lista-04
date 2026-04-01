159d004fc7489e9b45b1ecc2b0b18b435ee22179
Vinicius Lazaroti <viniciuslazarotti@gmail.com>
2026-04-01 14:28:31 -0300

    Alg2.java

--- algoritmos/Alg2.java
+++ algoritmos/Alg2.java
@@ -1,1 +1,23 @@
+import java.util.Scanner;
 
+public class Alg2 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        String user;
+        String senha; 
+        do {
+            System.out.println("Nome de usuário: ");
+            user = teclado.next();
+            System.out.println("Digite uma senha (apenas letras): ");
+            senha = teclado.next();
+            
+            if (user.equalsIgnoreCase(senha)) {
+                System.out.println("A senha não pode ser igual ao nome de usuário");
+            } 
+            
+        } while(user.equalsIgnoreCase(senha));
+        
+
+        teclado.close();
+    }
+}
