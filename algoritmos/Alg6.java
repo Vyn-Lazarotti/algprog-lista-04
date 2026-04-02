b6d28e3b1d6d21bb053e76722c479d8c9999bda4
Vinicius Lazaroti <viniciuslazarotti@gmail.com>
2026-04-02 09:07:56 -0300

    Alg6.java

--- algoritmos/Alg6.java
+++ algoritmos/Alg6.java
@@ -1,1 +1,9 @@
-
+public class Alg6 {
+  public static void main(String[] args) {
+    System.out.println("1 \n2 \n3 \n4 \n5 \n6 \n7 \n8 \n9 \n10 \n11 \n12 \n13 \n14 \n15 \n16 \n17 \n18 \n19 \n20");
+    System.out.println("1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20");
+    
+    //Sim Daniel, eu sei que era pra usar o for (int i = 0, i < 21, i++), mas essa solução é muito mais rápida e engraçada. Se tá funcionando, então não tá errado :D
+  }
+}
+    
