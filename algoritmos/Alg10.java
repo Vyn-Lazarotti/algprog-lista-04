f1c00586edd625d8ec938790dd00c526f511e4fe
Vinicius Lazaroti <viniciuslazarotti@gmail.com>
2026-04-03 09:11:57 -0300

    Alg10.java

--- algoritmos/Alg10.java
+++ algoritmos/Alg10.java
@@ -1,1 +1,21 @@
+import java.util.Scanner;
 
+public class Alg10 {
+  public static void main(String[] args) {
+    Scanner teclado = new Scanner(System.in);
+    
+    System.out.println("Digite um número inteiro: ");
+    int x = teclado.nextInt();
+    System.out.println("Digite outro número inteiro: ");
+    int y = teclado.nextInt();
+    
+    //System.out.println("Números entre eles:");
+
+    for (int i = x; i <= y; i++) {
+      System.out.println(i);
+    }
+
+    teclado.close();
+  }
+
+}
