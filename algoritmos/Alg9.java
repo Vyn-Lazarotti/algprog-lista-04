289489dff70ec55c3ae12561634a5fec2f4aaf15
Vinicius Lazaroti <viniciuslazarotti@gmail.com>
2026-04-03 08:52:50 -0300

    Alg9.java

--- algoritmos/Alg9.java
+++ algoritmos/Alg9.java
@@ -1,1 +1,9 @@
-
+public class Alg9 {
+	public static void main(String[] args) {
+			for(int num = 0; num <= 50; num++){ 
+		    if(num%2==1){ 
+			    System.out.println(num);
+        } 
+      } 
+	}
+}
